0b320113640f8d5660a18a02251579c27253802b
prututu <castd650@newschool.edu>
2023-04-25 21:47:28 -0400

    Result on a separate page

--- assets/main.js
+++ assets/main.js
@@ -46,10 +46,16 @@ date2.addEventListener('input', checkInputs);
 */
 
 function checkInputs() {
+
+  console.log('Are both imputs filled?');
+
   if (date1.value && date2.value) {
-    calculateDaysButton.disabled = false;
+      console.log('Yes.');
+      calculateDaysButton.disabled = false;
+    
   } else {
-    calculateDaysButton.disabled = true;
+      console.log('No. One or both inputs are empty.');
+      calculateDaysButton.disabled = true;
   }
 }
 
@@ -58,8 +64,36 @@ calculateDaysButton.addEventListener('click', calculateDaysDifference);
 
 /* STEP 4b Add a function that calculates the days */
 function calculateDaysDifference() {
-  const firstdate = moment(date1.value, "YYYY-MM-DD");
-  const seconddate = moment(date2.value, "YYYY-MM-DD");
-  const daysDifference = Math.abs(firstdate.diff(seconddate, 'days'));
-  result.textContent = daysDifference;
+
+    // Log the input field values
+    console.log("Date 1: " + date1.value);
+    console.log("Date 2: " + date2.value);
+
+    // Create 'moments' from input field values and log the formatted dates
+    const firstdate = moment(date1.value, "YYYY-MM-DD");
+    const seconddate = moment(date2.value, "YYYY-MM-DD");
+    
+    console.log("Moment Date 1: " + firstdate.format("YYYY MM DD"));
+    console.log("Moment Date 2: " + seconddate.format("YYYY MM DD"));
+
+    // Calculate the days difference between date1 and date2
+    const daysDifference = Math.abs(firstdate.diff(seconddate, 'days'));
+
+    // Log the days difference and display the result
+    console.log("Difference between days: " + daysDifference);
+    result.textContent = daysDifference;
+
+    document.getElementById('calculator-container').style.display = 'none';
+    document.getElementById('result-container').style.display = 'block';
+}
+
+/* --BACK BUTTON ON RESULTS "PAGE"-- */
+
+// Add an event listener for the Back button
+const backButton = document.getElementById('back');
+backButton.addEventListener('click', goBack);
+
+function goBack() {
+  document.getElementById('calculator-container').style.display = 'block';
+  document.getElementById('result-container').style.display = 'none';
 }
